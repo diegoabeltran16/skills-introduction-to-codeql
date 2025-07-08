95218603f98dd4f6f6b2d9a1373f65c7a220e813
Diego Alejandro Beltran <159483287+diegoabeltran16@users.noreply.github.com>
2025-07-08 18:08:38 -0400

    Update routes.py

--- server/routes.py
+++ server/routes.py
@@ -13,15 +13,15 @@ def index():
 
     if name:
         cursor.execute(
-            "SELECT * FROM books WHERE name LIKE '%" + name + "%'"
-        )
+            "SELECT * FROM books WHERE name LIKE %s",
+            ("%" + name + "%",)
         books = [Book(*row) for row in cursor]
 
     elif author:
         cursor.execute(
             "SELECT * FROM books WHERE author LIKE '%" + author + "%'"
-        )
-        books = [Book(*row) for row in cursor]
+            "SELECT * FROM books WHERE author LIKE %s",
+            ("%" + author + "%",)
 
     else:
         cursor.execute("SELECT name, author, read FROM books")
